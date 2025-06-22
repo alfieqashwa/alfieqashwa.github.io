326ae6ed60cad7fa43920ea12af6539442462b3f
Alfie Qashwa <alfieqashwa@gmail.com>
2025-06-22 20:22:54 +0700

    feat: setup tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://alfieqashwa.github.io",
   base: "/",
   output: "static",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
