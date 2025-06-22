2fcd0505586e905b5bd20734aa16b4b842272b2c
Alfie Qashwa <alfieqashwa@gmail.com>
2025-06-22 23:30:54 +0700

    feat: finished basic astro tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   site: "https://alfieqashwa.github.io",
   base: "/",
   output: "static",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
