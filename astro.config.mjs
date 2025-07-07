715ebb45da77730c239a55f8306c46a7f79b2378
Alfie Qashwa <alfieqashwa@gmail.com>
2025-07-08 00:11:06 +0700

    fix: reconfig decrypt post

--- astro.config.mjs
+++ astro.config.mjs
@@ -66,7 +66,9 @@ export default defineConfig({
       },
     }),
     mdx(),
-    react(),
+    react({
+      include: ["**/react/*"],
+    }),
     sitemap(),
     icon(),
   ],
--- src/components/react/Decrypt.tsx
+++ src/components/react/Decrypt.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useState } from "react";
 import CryptoJS from "crypto-js";
 import ReactMarkdown from "react-markdown";
